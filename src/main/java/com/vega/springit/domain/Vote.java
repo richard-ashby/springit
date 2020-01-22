65d57926517f9ec1308e6fa89d940e0f05ff0b7a
richard-ashby <richard.c.h.ashby@gmail.com>
2020-01-22 12:55:07 +0000

    Created our entities and refactored using Project Lombok

---
+++ src/main/java/com/vega/springit/domain/Vote.java
@@ -0,0 +1,25 @@
+package com.vega.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private int vote;
+
+    // user
+    // link
+
+}
