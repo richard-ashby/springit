50dc2a4860677491afd394a1455497e046858fa8
richard-ashby <richard.c.h.ashby@gmail.com>
2020-01-30 18:17:07 +0000

    Setting up our in memory database

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -1,8 +1,6 @@
 package com.vega.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +8,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
-@Data
+@RequiredArgsConstructor
+@Getter @Setter
 @NoArgsConstructor
 public class Comment extends Auditable {
 
@@ -21,6 +20,7 @@ public class Comment extends Auditable {
     private String body;
 
     @ManyToOne
+    @NonNull
     private Link link;
 
 }
