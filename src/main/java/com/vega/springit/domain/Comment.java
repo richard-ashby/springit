59b628fe37a6ed0623b682bbfd5c06ecdaeabd58
Richard Ashby <richard.c.h.ashby@gmail.com>
2020-08-13 17:05:07 +0100

    Added Comment listing capability

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -2,10 +2,18 @@ package com.vega.springit.domain;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
+import com.vega.springit.service.BeanUtil;
 
 @Entity
 @RequiredArgsConstructor
@@ -16,6 +24,7 @@ public class Comment extends Auditable {
     @Id
     @GeneratedValue
     private Long id;
+    
     @NonNull
     private String body;
 
@@ -23,4 +32,13 @@ public class Comment extends Auditable {
     @NonNull
     private Link link;
 
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
+    
 }
